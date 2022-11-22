fb914079f5d340020bd71f9487d8b7ee2beaa568
root <root@debian>
2022-11-22 18:52:50 -0500

    header adicionado

---
+++ src/styled/components/Header/Header.js
@@ -0,0 +1,16 @@
+import React from "react";
+import * as C from "./styled";
+
+
+const Header = () => {
+
+    return(
+        <>
+
+            <C.Teste>Header</C.Teste>
+            
+        </>
+    )
+};
+
+export default Header;
---
+++ src/styled/components/Header/styled.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const Teste = styled.h1`
+
+font-size: 1.5em;
+align-items: flex-start;
+color: palevioletred;
+background: #fff;
+
+`;
